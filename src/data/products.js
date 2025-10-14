4472a1186194f7d5deb58b4fa32212f0c3c4934f
michdiazf <mich.diazf@duocuc.cl>
2025-10-14 01:32:56 -0300

    chore: resolver conflictos de rebase y agregar imágenes .web

--- src/data/products.js
+++ src/data/products.js
@@ -4,7 +4,7 @@ const products = [
         name: "Hills Canine Metabolic + Mobility",
         description: "Es comida de perro 1",
         price: 169990,
-        image: 'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dw3092e7a9/images/1221_m1.jpg',
+        image: "/images/1-hills-canine-metabolic-mobility.webp",
         tipo: 'perro'
     },
     {
@@ -12,7 +12,7 @@ const products = [
         name: "Hills Canine J/D Joint Care",
         description: "Es comida de perro 2",
         price: 159990,
-        image: 'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dwf21ef611/images/151_m1.jpg',
+        image: "/images/2-hills-canine-jd-joint-care.webp",
         tipo: 'perro'
     },
     {
@@ -20,7 +20,7 @@ const products = [
         name: "Hills Canine Metabolic",
         description: "Es comida de perro 3",
         price: 149990,
-        image: 'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dwf1387bed/images/153_m1.jpg',
+        image: "/images/3-hills-canine-metabolic.webp",
         tipo: 'perro'
     },
     {
@@ -28,7 +28,7 @@ const products = [
         name: "Acana Singles Duck & Pear alimento para perro",
         description: "Es alimento de perro 4",
         price: 121990,
-        image: 'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dw50c83657/images/58a34-ds-acana-singles-dog-duck-pear-recipe-front-25lb.jpg',
+        image: "/images/4-acana-singles-duck-pear-alimento-para-perro.webp",
         tipo: 'perro'
     },
     {
@@ -36,7 +36,7 @@ const products = [
         name: "Hills Canine Adult Large Breed alimento para perro",
         description: "Es comida de perro 5",
         price: 119990,
-        image: 'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dw2b5f14b9/images/ac423-adult-1-5-large-breed.jpg',
+        image: "/images/5-hills-canine-adult-large-breed-alimento-para-perro.webp",
         tipo: 'perro'
     },
     {
@@ -44,7 +44,7 @@ const products = [
         name: "Orijen Original Dog alimento para perro",
         description: "Es comida de perro 6",
         price: 119990,
-        image: 'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dw6a964dbc/images/b060f-ds-orijen-dog-original-fr-lg.jpg',
+        image: "/images/6-orijen-original-dog-alimento-para-perro.webp",
         tipo: 'perro'
     },
     {
@@ -52,7 +52,7 @@ const products = [
         name: "Orijen Six Fish Cat alimento para gato",
         description: "Es comida de gato 1",
         price: 99990,
-        image: 'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dwca353c50/images/118.png',
+        image: "/images/7-orijen-six-fish-cat-alimento-para-gato.webp",
         tipo: 'gato'
     },
     {
@@ -60,7 +60,7 @@ const products = [
         name: "Orijen Fit & Trim Cat alimento para gato",
         description: "Es comida de gato 2",
         price: 99990,
-        image: 'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dwc979e9d9/images/920d7-web.jpg',
+        image: "/images/8-orijen-fit-trim-cat-alimento-para-gato.webp",
         tipo: 'gato'
     },
     {
@@ -68,7 +68,7 @@ const products = [
         name: "Orijen Original cat alimento seco completo para gatos",
         description: "Es comida de gato 3",
         price: 95990,
-        image: 'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dwad977271/images/1317_m1.jpg',
+        image: "/images/9-orijen-original-cat-alimento-seco-completo-para-gatos.webp",
         tipo: 'gato'
     },
     {
@@ -76,7 +76,7 @@ const products = [
         name: "N&D Quinoa Feline Adult Skin & Coat Peixe alimento para gato",
         description: "Es comida de gato 4",
         price: 89990,
-        image: 'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dwf16d9f7c/images/609_m1.jpg',
+        image: "/images/10-nd-quinoa-feline-adult-skin-coat-peixe-alimento-para-gato.webp",
         tipo: 'gato'
     },
     {
@@ -84,7 +84,7 @@ const products = [
         name: "Hills Feline Adult Optimal Care Original alimento para gato",
         description: "Es comida de gato 5",
         price: 35990,
-        image: 'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dwd94a0c82/images/1264_m1.jpg',
+        image: "/images/11-hills-feline-adult-optimal-care-original-alimento-para-gato.webp",
         tipo: 'gato'
     },
     {
@@ -92,7 +92,7 @@ const products = [
         name: "Leonardo Adult Salmon Gf alimento para gato",
         description: "Es comida de gato 6",
         price: 22990,
-        image: 'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dwb0789dcb/images/2416_m.jpg',
+        image: "/images/12-leonardo-adult-salmon-gf-alimento-para-gato.webp",
         tipo: 'gato'
     },
     {
@@ -100,7 +100,7 @@ const products = [
         name: "Leeby Cojín Impermeable Anti Pelos Rojo para perros",
         description: "Es un accesorio 1",
         price: 29990,
-        image:'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dwc88c327f/images/8097_m-1.jpg',
+        image: "/images/13-leeby-cojin-impermeable-anti-pelos-rojo-para-perros.webp",
         tipo: 'accesorio'
     },
     {
@@ -108,7 +108,7 @@ const products = [
         name: "Coto black loop ball",
         description: "Es un accesorio 2",
         price: 6990,
-        image:'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dwaa454e36/images/Coto%20black%20loop%20ball.jpg',
+        image: "/images/14-coto-black-loop-ball.webp",
         tipo: 'accesorio'
     },
     {
@@ -116,7 +116,7 @@ const products = [
         name: "Outech cinturón manos libres de paseo",
         description: "Es un accesorio 3",
         price: 10500,
-        image:'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dwc4a5647e/images/Cinturon%20Manos%20Libres%20De%20Paseo.jpg',
+        image: "/images/15-outech-cinturon-manos-libres-de-paseo.webp",
         tipo: 'accesorio'
     },
     {
@@ -124,7 +124,7 @@ const products = [
         name: "Rascador monica",
         description: "Es un accesorio 4",
         price: 119000,
-        image:'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dw19d231bc/images/Rascador%20monica%202.jpg',
+        image: "/images/16-rascador-monica.webp",
         tipo: 'accesorio'
     },
     {
@@ -132,7 +132,7 @@ const products = [
         name: "La caña serpiente unidad",
         description: "Es un accesorio 1",
         price: 2990,
-        image:'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dwb52a30e3/images/a45fd-8436569704233.1-2-.jpg',
+        image: "/images/17-la-cana-serpiente-unidad.webp",
         tipo: 'accesorio'
     },
     {
@@ -140,7 +140,7 @@ const products = [
         name: "Zeecat collar skull 2.0 para gato",
         description: "Es un accesorio 1",
         price: 5990,
-        image:'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dw07b61f17/images/c9dbd-zeedog-collar-skull-2.0-para-gato-1-.jpg',
+        image: "/images/18-zeecat-collar-skull-2-0-para-gato.webp",
         tipo: 'accesorio'
     },
 ];
